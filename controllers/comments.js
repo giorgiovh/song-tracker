d66f333d1817701c3cb541de25b49b2c83df8ed4
giorgiovh <jorgevonhoroch@gmail.com>
2021-10-16 22:45:08 -0400

    creates the create (for creating a comment) function

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,2 +1,24 @@
 import { Song } from './../models/song.js'
+import { Comment } from './../models/comment.js'
 
+function create(req, res) {
+    req.body.author = req.user.profile._id
+    req.body.song = req.params.id
+
+    Comment.create(req.body)
+    .then(comment => {
+        Song.findById(req.params.id)
+        .then(song => {
+            song.comments.push(comment._id)
+            song.save()
+            .then(() => {
+                res.redirect(`songs/${song._id}`)
+            })
+        })
+    })
+
+}
+
+export {
+    create
+}

dc1f3f93cb445c09c537b8d36a42e2b9bc19b5a6
giorgiovh <jorgevonhoroch@gmail.com>
2021-10-17 17:39:09 -0400

    adds comment content on newly added comments

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,6 @@ import { Song } from './../models/song.js'
 import { Profile } from './../models/profile.js'
 import { Comment } from './../models/comment.js'
 
-// function index(req, res) {
-//     res.render('index', {
-//         title: 'Song Tracker',
-//         user: req.user? req.user : null
-//     })
-// }
-
 function index(req, res) {
     Song.find({})
     .sort({_id: -1})

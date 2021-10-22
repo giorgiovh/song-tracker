b1f9d60e49b5e28a7b463640b16d1e37276200f7
giorgiovh <jorgevonhoroch@gmail.com>
2021-10-21 23:26:08 -0400

    removes imageUrl field

--- models/comment.js
+++ models/comment.js
@@ -3,11 +3,18 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
-    song: {type: Schema.Types.ObjectId, ref: 'Song'},
-    author: {type: Schema.Types.ObjectId, ref: 'Profile'
+    song: {
+        type: Schema.Types.ObjectId, 
+        ref: 'Song'
     },
-    content: {type: String
-    }}, {
+    author: {
+        type: Schema.Types.ObjectId, 
+        ref: 'Profile'
+    },
+    content: {
+        type: String
+    }
+}, {
     timestamps: true
 })
 
